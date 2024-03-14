1df8bf3dd473b863e214ffad7e2bd7968cef6ef7
Clav2k1 <clav2k1@hotmail.com>
2024-03-13 21:59:55 -0300

    Deploy API to FL0

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const crypto = require('node:crypto')  //-> permite crear id unicas
+const movies = require('./movies.json')
+const cors = require('cors')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:3000',
+      'https://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Invalid origin'))
+  }
+}))
+app.disable('x-powered-by')
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()));
+    return res.json(filteredMovies)
+  }
+
+  return res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) =>{
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  
+  if (movie) return res.json(movie)
+
+  return res.status(404).json({ message: 'Movie Not Found' })
+})
+
+app.post('/movies', (req, res) =>{
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+  
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+
+  return res.status(201).json(newMovie)  
+})
+
+app.patch('/movies/:id', (req, res) =>{
+  const result = validatePartialMovie(req.body)
+  if (result.error) return res.status(400).json({ error: JSON.parse(result.error.message) })
+  
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) return res.status(404).json({ message: 'Movie Not Found' })
+
+  const updateMovie = {...movies[movieIndex], ...result.data}
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) =>{
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) return res.status(404).json({ message: 'Movie Not Found' })
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({message: 'Movie deleted'})
+})
+
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, ()=> {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
